050054350f07fb6f79f7322829ab6b8e6c5fd805
yoshikawakyosuke <rq6256@gmail.com>
2021-09-14 07:33:56 +0900

    ログイン機能の実装

---
+++ practice1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>鬼練１</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+$answer = '仕事に決まってるだろ！';
+
+echo '彼女：仕事と私どっちを取るのよ！<br>';
+echo '自分：' . $answer . '<br>';
+
+if($answer === '仕事に決まってるだろ！'){
+  echo '彼女：あたたたたたたたっーーー！！<br>';
+  echo '自分：ひでぶっ！！';
+}
+
+?>
+
+
+
+</body>
+</html>
